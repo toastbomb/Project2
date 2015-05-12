a9286e319e7fcb8652b103327717e8069187c7ed
Tucker Kolpin <tckolpin@uorgeon.edu>
2015-05-12 14:56:20 -0700

    Added a bunch of art stuff

---
+++ Projects/Project2/Assets/Editor/NoFBXMaterials.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class NoFBXMaterials : AssetPostprocessor
+{
+	void OnPreprocessModel()
+	{
+		ModelImporter importer = assetImporter as ModelImporter;
+		importer.importMaterials = false;
+	}
+}
