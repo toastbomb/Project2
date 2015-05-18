4633c7db4e4737ec4f2da60babfcfcaf41f521fd
Tucker Kolpin <tckolpin@uorgeon.edu>
2015-05-17 23:26:18 -0700

    Added dialogue boxes and started UI

---
+++ Projects/Project2/Assets/_Scripts/NPC.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class NPC : MonoBehaviour 
+{
+	public string[] conversation;
+	private List<string> conversationList = new List<string>();
+	private bool inRangeOfPlayer = false;
+
+	void Start () 
+	{
+		foreach(string text in conversation)
+		{
+			conversationList.Add(text);
+		}
+	}
+	void Update () 
+	{
+		if(inRangeOfPlayer)
+		{
+			if(Input.GetKeyUp(KeyCode.E))
+			{
+				TextBox.textBox.StartTalking(new List<string>(conversationList));
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			inRangeOfPlayer = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			inRangeOfPlayer = false;
+		}
+	}
+}
---
+++ Projects/Project2/Assets/_Scripts/TextBox.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using System.Collections;
+
+public class TextBox : MonoBehaviour 
+{
+	public static TextBox textBox;
+	private List<string> dialogues = new List<string>();
+	public Text dialogue;
+
+	void Awake () 
+	{
+		if(textBox == null)
+		{
+			textBox = this;
+		}
+		else if(textBox != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void Start()
+	{
+		HideTextBox();
+	}
+
+	void OnDisable()
+	{
+		DialogueLoop();
+	}
+
+	public void NextText()
+	{
+		DialogueLoop();
+	}
+
+	private void DialogueLoop()
+	{
+		if(dialogues != null)
+		{
+			if(dialogues.Count > 0)
+			{
+				GiveNewDialogue(dialogues[0]);
+				dialogues.RemoveAt(0);
+				ShowTextBox();
+			}
+			else
+			{
+				HideTextBox();
+			}
+		}
+	}
+
+	private void GiveNewDialogue(string someText)
+	{
+		dialogue.text = someText;
+	}
+
+	private void ShowTextBox()
+	{
+		this.gameObject.SetActive(true);
+	}
+
+	private void HideTextBox()
+	{
+		this.gameObject.SetActive(false);
+	}
+
+	public void StartTalking(List<string> dial)
+	{
+		dialogues.Clear();
+		dialogues = dial;
+		DialogueLoop();
+	}
+}
